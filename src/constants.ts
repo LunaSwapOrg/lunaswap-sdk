dc6cec747e732323e8aaf4d90fa0163997bf9e16
markdavid000 <davidojukwu59@gmail.com>
2024-08-28 11:36:03 +0100

    sdk modified

--- src/constants.ts
+++ src/constants.ts
@@ -8,7 +8,8 @@ export enum ChainId {
   ROPSTEN = 3,
   RINKEBY = 4,
   GÖRLI = 5,
-  KOVAN = 42
+  KOVAN = 42,
+  BASE_SEPOLIA = 84532
 }
 
 export enum TradeType {
@@ -22,9 +23,9 @@ export enum Rounding {
   ROUND_UP
 }
 
-export const FACTORY_ADDRESS = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'
+export const FACTORY_ADDRESS = '0xD40514b8Ae67Ac9A8B9e5d4048e684612f762DD4'
 
-export const INIT_CODE_HASH = '0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f'
+export const INIT_CODE_HASH = '0xe6fcaca4abd911e6ac3b3b73da39066a14962d58bd2f52c3015393b0756402c9'
 
 export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
 
